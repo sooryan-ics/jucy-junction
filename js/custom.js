e398afd3abb07a5573eef7bb419f7058cc55973c
sooryan-ics <sooryanv04@gmail.com>
2026-05-08 12:58:11 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // to get current year
 function getYear() {
-    var currentDate = new Date();
+    const currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     document.querySelector("#displayYear").innerHTML = currentYear;
 }
